05539ca0edcbe972faae7c8ef74133c72e269e2b
Jon Moses <jon@burningbush.us>
2018-05-15 08:24:42 -0400

    Upgraded fzf

--- fzf.bash
+++ fzf.bash
@@ -1,18 +1,12 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" =~ "/usr/local/opt/fzf/bin" ]]; then
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" =~ "/usr/local/opt/fzf/man" && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
