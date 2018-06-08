e30512a52821635d4b15c72c6c3930515131ed31
Jon Moses <jon@burningbush.us>
2018-06-08 08:11:32 -0400

    More bins

--- bin/g-fetch.sh
+++ bin/g-fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-exec wget --user-agent "Googlebot/2.1 (+http://www.googlebot.com/bot.html)" -c --tries 2 "$@"
+exec wget --user-agent "Googlebot/2.1 (+http://www.googlebot.com/bot.html)" -c --tries 2 --connect-timeout 10 --read-timeout 10 "$@"
