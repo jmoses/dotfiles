9db3a618fac7ca91b75d6c3e15b16eb73de8a115
Jon Moses <jon@burningbush.us>
2022-04-06 11:27:41 -0400

    feat(secrets): functions to clear and reload

--- zsh/.zsh.d/env-secrets.sh
+++ zsh/.zsh.d/env-secrets.sh
@@ -2,35 +2,41 @@ cachetarget=${SHELL_CACHED_SECRETS_FILE:-${HOME}/.zsh.secrets.cached}
 vault=${SHELL_CACHED_SECRETS_VAULT:-Private}
 itemname=${SHELL_CACHED_SECRETS_ITEM:-ENV_VARS}
 
-if [ -e $cachetarget ] ; then
-    if ! stat $cachetarget | grep '\-rw-------' &> /dev/null ; then
-        echo "Incorrect permissions on cached secrets file"
-    fi
-    source $cachetarget
-elif [[ -o interactive ]] ; then
-    if read -sqt5 "x?Download and cache secrets? " ; then
-        op list vaults --cache &> /dev/null
-        SIGNED_IN=$?
-        # TODO: A new session will never be signed in, how do?
-        if [ ! $SIGNED_IN -eq 0 ]  ; then 
-            echo 
-            if read -sqt5 "x?Not signed into 1password, signin? " ; then
-                echo
-                eval $(op signin logdna)
-                op list vaults --cache &> /dev/null
-                SIGNED_IN=$?
+function _load_or_cache_secrets {
+    if [ -e $cachetarget ] ; then
+        if ! stat $cachetarget | grep '\-rw-------' &> /dev/null ; then
+            echo "Incorrect permissions on cached secrets file"
+        fi
+        source $cachetarget
+    elif [[ -o interactive ]] ; then
+        if read -sqt5 "x?Download and cache secrets? " ; then
+            op list vaults --cache &> /dev/null
+            SIGNED_IN=$?
+            # TODO: A new session will never be signed in, how do?
+            if [ ! $SIGNED_IN -eq 0 ]  ; then 
+                echo 
+                if read -sqt5 "x?Not signed into 1password, signin? " ; then
+                    echo
+                    eval $(op signin logdna)
+                    op list vaults --cache &> /dev/null
+                    SIGNED_IN=$?
+                fi
+            fi
+
+            if [ $SIGNED_IN -eq 0 ] ; then
+                touch $cachetarget
+                chmod 600 $cachetarget
+                op get item --cache --vault $vault ${itemname} | jq -r '.details.sections | .[] | .fields | .[]  | "export " + .t + "=" + .v' > $cachetarget
+                source $cachetarget
+            else
+                echo "Not signed into 1p."
             fi
         fi
-
-        # > op get item --vault Private ENV_VARS | jq '.details.sections | .[] | .fields | .[]  | .t + "=" + .v'
-
-        if [ $SIGNED_IN -eq 0 ] ; then
-            touch $cachetarget
-            chmod 600 $cachetarget
-            op get item --cache --vault $vault ${itemname} | jq -r '.details.sections | .[] | .fields | .[]  | "export " + .t + "=" + .v' > $cachetarget
-            source $cachetarget
-        else
-            echo "Not signed into 1p."
-        fi
     fi
-fi
+}
+
+function clear_env_secrets {
+    rm $cachetarget
+}
+
+_load_or_cache_secrets
