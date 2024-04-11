5ad1e0d92e733693da5524a031fe096f23457822
Jon Moses <jon@burningbush.us>
2024-04-11 15:17:33 -0400

    shellcheck

--- firefox/hide-pinned-activity-indicator.sh
+++ firefox/hide-pinned-activity-indicator.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-css = << CSS
-@-moz-document url(chrome://browser/content/browser.xhtml) {
-  /* https://www.userchrome.org/firefox-changes-userchrome-css.html#fx69 */
-  .tabbrowser-tab > .tab-stack > .tab-content[pinned][titlechanged]:not([selected="true"]) {
-  
-    background-image: none !important;
-  }
-  
-  .tabbrowser-tab[label*="WhatsApp"] > .tab-stack >
-  .tab-content[pinned][titlechanged]:not([selected="true"]){
-    border-bottom: 2px solid rgba(0, 0, 255, 0.4);
-  }
-  
-}
-CSS
-
 profile_dir=''
-if [ $(uname -s) = 'Darwin' ] ; then 
-    name=$(ls ${HOME}/Library/Application\ Support/Firefox/Profiles/ | grep default-release)
-    if [ -x $name ] ; then
+if [ "$(uname -s)" = 'Darwin' ] ; then
+    # shellcheck disable=SC2010
+    name=$(ls "${HOME}/Library/Application Support/Firefox/Profiles/" | grep default-release)
+    if [ -x "$name" ] ; then
         echo "Can't find profile"
         exit 1
     fi
@@ -37,22 +22,22 @@ fi
 userChrome="${profile_dir}/chrome/userChrome.css"
 if [[ ! -f $userChrome ]] ; then
     echo "Creating userchrome file"
-    mkdir -p $(dirname "${userChrome}")
+    mkdir -p "$(dirname "${userChrome}")"
     cat > "${userChrome}" << CSS
 @-moz-document url(chrome://browser/content/browser.xhtml) {
   /* https://www.userchrome.org/firefox-changes-userchrome-css.html#fx69 */
   .tabbrowser-tab > .tab-stack > .tab-content[pinned][titlechanged]:not([selected="true"]) {
-  
+
     background-image: none !important;
   }
-  
+
   .tabbrowser-tab[label*="WhatsApp"] > .tab-stack >
   .tab-content[pinned][titlechanged]:not([selected="true"]){
     border-bottom: 2px solid rgba(0, 0, 255, 0.4);
   }
-  
+
 }
 CSS
-else 
+else
     echo "userChrome already exists"
 fi
